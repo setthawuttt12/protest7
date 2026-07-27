a7f6ae9de619021b5ccdfdf2108e83aad3581486
bird01 <bird01@vecskill.ovec>
2026-07-27 12:33:23 +0700

    Score_member[id_eva]

--- backend/index.js
+++ backend/index.js
@@ -26,5 +26,9 @@ const edit = require('./routes/Eva/edit_eva')
 app.use('/api/Eva/edit_eva',edit)
 
 
+//commit
+const score_member2 = require('./routes/Commit/score_member')
+app.use('/api/Commit/score_member',score_member2)
+
 app.use((req,res)=> res.status(404).json({message:'404 invalid Route77'}))
 app.listen(3001, ()=> console.log("Server Runnig in Port 3001✅"))
