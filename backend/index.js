15e182a0f07cb09d089e6e537c959da7352adf77
yootoshima <yootoshi53258@gmail.com>
2026-07-27 15:22:16 +0700

    update Eva

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,12 @@ app.use('/api/auth',auth)
 const edit = require('./routes/Eva/edit_eva')
 app.use('/api/Eva/edit_eva',edit)
 
+const self = require('./routes/Eva/selfeva')
+app.use('/api/Eva/selfeva',self)
+
+const score_m = require('./routes/Eva/score_member')
+app.use('/api/Eva/score_member',score_m)
+
 
 app.use((req,res)=> res.status(404).json({message:'404 invalid Route77'}))
 app.listen(3001, ()=> console.log("Server Runnig in Port 3001✅"))
